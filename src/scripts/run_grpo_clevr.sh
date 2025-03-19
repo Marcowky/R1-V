a1acaaf46c8e51af7057ebb5867c51ee76c9cf18
Marco <1341887814@qq.com>
2025-03-19 19:52:45 +0800

    add: 添加了 Weather GRPO 的代码

--- src/scripts/run_grpo_clevr.sh
+++ src/scripts/run_grpo_clevr.sh
@@ -13,16 +13,16 @@ torchrun --nproc_per_node="1" \
     --output_dir /home/kaiyu/Graduation/REF_REPOS/R1-V/output \
     --model_name_or_path /home/kaiyu/Model/Qwen/Qwen2-VL-2B-Instruct \
     --dataset_name leonardPKU/GEOQA_R1V_Train_8K \
-    --max_prompt_length 1024 \
+    --deepspeed local_scripts/zero3.json \
     --per_device_train_batch_size 1 \
-    --gradient_accumulation_steps 1 \
+    --gradient_accumulation_steps 2 \
     --logging_steps 1 \
     --bf16 \
     --report_to wandb \
     --gradient_checkpointing false \
     --attn_implementation flash_attention_2 \
-    --max_pixels 401408 \
     --num_train_epochs 2 \
     --run_name Qwen2-VL-2B-GRPO-CLEVR-70k \
     --save_steps 100 \
-    --save_only_model true
+    --save_only_model true \
+    --num_generations 8
--- src/scripts/run_grpo_weatherrft.sh
+++ src/scripts/run_grpo_weatherrft.sh
@@ -1,19 +1,19 @@
-cd src/r1-v/
+cd src/r1-v
 
 export DEBUG_MODE="true"
 export LOG_PATH="./debug_log_2b.txt"
 export HF_ENDPOINT='https://hf-mirror.com'
 
-torchrun --nproc_per_node="1" \
+torchrun --nproc_per_node="8" \
     --nnodes="1" \
     --node_rank="0" \
     --master_addr="127.0.0.1" \
     --master_port="12345" \
-    src/open_r1/grpo_weather.py \
+    src/open_r1/grpo_weatherrft.py \
     --output_dir /home/kaiyu/Graduation/REF_REPOS/R1-V/output \
     --model_name_or_path /home/kaiyu/Model/Qwen/Qwen2.5-VL-3B-Instruct \
     --dataset_name leonardPKU/GEOQA_R1V_Train_8K \
-    --max_prompt_length 1024 \
+    --deepspeed local_scripts/zero3.json \
     --per_device_train_batch_size 1 \
     --gradient_accumulation_steps 2 \
     --logging_steps 1 \
@@ -21,8 +21,8 @@ torchrun --nproc_per_node="1" \
     --report_to wandb \
     --gradient_checkpointing false \
     --attn_implementation flash_attention_2 \
-    --max_pixels 401408 \
     --num_train_epochs 2 \
-    --run_name Qwen2-VL-2B-GRPO-CLEVR-70k \
+    --run_name Qwen2.5-VL-3B-GRPO-WeatherRFT \
     --save_steps 100 \
-    --save_only_model true
+    --save_only_model true \
+    --num_generations 8
